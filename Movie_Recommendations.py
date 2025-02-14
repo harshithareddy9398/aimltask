91a24472106cde791e1fbec80e6643d7f8669635
harshithareddy9398 <2311cs020010@mallareddyuniversity.ac.in>
2025-02-14 12:58:00 +0530

    Add files via upload

---
+++ Movie_Recommendations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# In[2]:
+
+
+movies = pd.read_csv("Movie.csv")
+movies
+
+
+# In[ ]:
+
+
+
+
